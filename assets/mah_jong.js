eaf318d2ca9926a68a505d0d318231cf1d125b01
SebiZeti <sebestyen.varga@gmail.com>
2023-08-15 15:59:18 +0200

    Redone everything

--- assets/mah_jong.js
+++ assets/mah_jong.js
@@ -1,10 +1,26 @@
-let selected = undefined; //indexed from 0
-let added_tiles = [[[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []]]; // 3, 3, 3, 3, 2, last list for flowers,
+let selected = undefined; //indexed from 0 
 let suits = ["b", "c", "r"]; //bamboo, character, round(circle)
 data = undefined
 
+// 3, 3, 3, 3, 2, last list for flowers
+let added_tiles = [...Array(4)].map(_ => [3, 3, 3, 3, 2, 8].map(l => [...Array(l)]));
+
 function init() {
     data = this.$data;
+}
+
+function startGame() {
+    for (const td of Array.from(document.querySelectorAll("td"))) {
+        td.addEventListener("drop", drop);
+        td.addEventListener("dragover", (ev) => ev.preventDefault());
+        td.addEventListener("dragenter", (ev) => td.classList.add("hovered"));
+        td.addEventListener("dragleave", (ev) => td.classList.remove("hovered"));
+    }
+
+    for (const img of Array.from(document.querySelectorAll("img"))) {
+        img.addEventListener("dragstart", drag);
+    }
+    console.log("started");
 }
 
 //Passes names and tiles to Alpine x-data
@@ -12,87 +28,62 @@ function pushAlpine(){
     data.names = [data.name1, data.name2, data.name3, data.name4];
     data.tiles = tiles;
 }
-
-//Select player to add tile to, deselect previous selected
-function select(player){
-    selected = player;
-    console.log(selected);
-    for(let i = 0; i < 4; i++){
-        if(i == selected){
-            document.getElementById("ply".concat(i.toString())).classList.add("selected");
-        }
-        else if(document.getElementById("ply".concat(i.toString())).classList.contains("selected")){
-            document.getElementById("ply".concat(i.toString())).classList.remove("selected");
-        }
+  
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+  
+function drop(ev) {
+    ev.target.classList.remove("hovered");
+    var data = ev.dataTransfer.getData("text");
+    if (ev.target.parentElement.classList.contains("flowerRow") && data.charAt(0) != "f") {
+        return;
     }
+    if (!ev.target.parentElement.classList.contains("flowerRow") && data.charAt(0) == "f") {
+        return;
+    }
+    ev.preventDefault();
+    var img = document.getElementById(data);
+    if (img.getAttribute("x-ignore")) {
+        var [ply_idx, row_idx, col_idx] = findTd(img.parentElement);
+        added_tiles[ply_idx][row_idx][col_idx] = undefined;
+    }    
+    else{
+        img = img.cloneNode();
+        img.addEventListener("dragstart", drag);
+        img.addEventListener("click", removeImg);
+        img.setAttribute("x-ignore", "true");
+    }
+
+    var [ply_idx, row_idx, col_idx] = findTd(ev.target);
+    ev.target.appendChild(img);
+    img.id = "tableimg_" + ply_idx + "_" + col_idx + "_" + row_idx;
+
+    added_tiles[ply_idx][row_idx][col_idx] = img.getAttribute("data-tile");
+    console.log(added_tiles);
 }
 
-//Adds tile with ident to selected player
-function addTile(ident){
-    console.log(ident);
-    if(selected != undefined){
-        if(ident.charAt(0) == "f"){
-            added_tiles[selected][5].push(ident);
-            addImgToTable(ident, selected, added_tiles[selected][5].length % 3 + 1, parseInt(added_tiles[selected][5].length/3) + 5);   
-        }
-        else{
-            for(let i = 0; i < 5; i++){
-                if(added_tiles[selected][i].length < 3 && i != 5){
-                    added_tiles[selected][i].push(ident);
-                    addImgToTable(ident, selected, added_tiles[selected][i].length - 1, i);
-                    break;
-                }
-                else if(added_tiles[selected][i][0] == added_tiles[selected][i][1] && added_tiles[selected][i][0] == added_tiles[selected][i][2] && added_tiles[selected][i][0] == ident && added_tiles[selected][i].length < 4 && i != 5){
-                    added_tiles[selected][i].push(ident);
-                    addImgToTable(ident, selected, 1, i, true)
-                    break;
-                }
-                else if(added_tiles[selected][i].length < 2 && i == 5){
-                    added_tiles[selected][i].push(ident);
-                    addImgToTable(ident, selected, added_tiles[selected][i].length - 1, i);
-                    break;
-                }
+function findTd(td) {
+    var ret = undefined;
+    Array.from(document.querySelectorAll(".player")).forEach((ply, ply_idx) => {
+        Array.from(ply.querySelector("table").children[0].children).forEach((row, row_idx) => {
+            var col_idx = Array.from(row.children).indexOf(td);
+            if (col_idx != -1) {
+                ret = [ply_idx, row_idx, col_idx];
             }
-        }
+        })
+    })
+    if (ret) {
+        return ret;
     }
-    console.log(added_tiles[selected]);
-}
-
-//Adds the image of the tile to the given x,y cords on the player's table, rot = true if it has to be rotated
-function addImgToTable(tile, ply, x, y, rot = false){
-    console.log(x + " " + y);
-    var cords = getTableCords(ply);
-    var div = document.getElementById("images");
-    var img = document.createElement("img");
-    img.src = "assets/images/" + tile + ".png";
-    img.id = "tableimg_" + ply + "_" + x + "_" + y;
-    img.classList.add("table_tile");
-    img.style.left = cords[0] + x * 85 + "px";
-    img.style.top = cords[1] + y * 125 + "px";
-    console.log(y + " * 125 " + " + " + cords[1] + " " + ply)
-    img.setAttribute("x-on:click", "removeImg(" + ply + "," + x + "," + y + ")")
-    if(rot){
-        img.style.transform = "rotate(90deg)";
-        img.style.zIndex = "100";
-        img.id = "tableimg_" + ply + "_" + 3 + "_" + y;
-        img.setAttribute("x-on:click", "removeImg(" + ply + "," + 3 + "," + y + ")")
-    }
-    div.appendChild(img);
-}
-
-//Gets the top and left edges of the table
-function getTableCords(id){
-    var table = document.getElementById("table" + id);
-    var bound = table.getBoundingClientRect();
-    return [bound.left, bound.top];
+    throw Error("Oh no!");
 }
 
 //Removes tile from given player at given x,y cords
-function removeImg(ply, x, y){
-    console.log(x + " " + y)
-    img = document.getElementById("tableimg_" + ply + "_" + x + "_" + y);
-    div = document.getElementById("images");
-    div.removeChild(img);
-    added_tiles[ply][y].splice(x,1);
-    console.log(added_tiles[selected]);
+function removeImg(ev){
+    const img = ev.target;
+    const [ply, y, x] = findTd (img.parentElement);
+    img.remove();
+    added_tiles[ply][y][x] = undefined;
+    console.log(added_tiles[ply]);
 }
