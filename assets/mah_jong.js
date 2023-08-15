e1e0664f265fc25e9eea75e298dddd73d90b04c3
SebiZeti <sebestyen.varga@gmail.com>
2023-08-15 10:46:11 +0200

    Began flower adding, position bug to be fixed

--- assets/mah_jong.js
+++ assets/mah_jong.js
@@ -33,6 +33,7 @@ function addTile(ident){
     if(selected != undefined){
         if(ident.charAt(0) == "f"){
             added_tiles[selected][5].push(ident);
+            addImgToTable(ident, selected, added_tiles[selected][5].length % 3 + 1, parseInt(added_tiles[selected][5].length/3) + 5);   
         }
         else{
             for(let i = 0; i < 5; i++){
@@ -54,10 +55,12 @@ function addTile(ident){
             }
         }
     }
+    console.log(added_tiles[selected]);
 }
 
 //Adds the image of the tile to the given x,y cords on the player's table, rot = true if it has to be rotated
 function addImgToTable(tile, ply, x, y, rot = false){
+    console.log(x + " " + y);
     var cords = getTableCords(ply);
     var div = document.getElementById("images");
     var img = document.createElement("img");
@@ -66,6 +69,7 @@ function addImgToTable(tile, ply, x, y, rot = false){
     img.classList.add("table_tile");
     img.style.left = cords[0] + x * 85 + "px";
     img.style.top = cords[1] + y * 125 + "px";
+    console.log(y + " * 125 " + " + " + cords[1] + " " + ply)
     img.setAttribute("x-on:click", "removeImg(" + ply + "," + x + "," + y + ")")
     if(rot){
         img.style.transform = "rotate(90deg)";
@@ -85,10 +89,10 @@ function getTableCords(id){
 
 //Removes tile from given player at given x,y cords
 function removeImg(ply, x, y){
-    console.log(x + " " +y)
+    console.log(x + " " + y)
     img = document.getElementById("tableimg_" + ply + "_" + x + "_" + y);
     div = document.getElementById("images");
     div.removeChild(img);
     added_tiles[ply][y].splice(x,1);
-    console.log(added_tiles);
+    console.log(added_tiles[selected]);
 }
